7d835a7980a017c4ad3ee96f1001864520139465
Jules Mattioni <julesmattioni@live.fr>
2025-11-04 13:10:03 +0100

    first commit

---
+++ get_next_line_utils.c
@@ -0,0 +1,72 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*dest;
+	size_t	total_size;
+    int i;
+
+    i = 0;
+	if (size && nmemb > (size_t) -1 / size)
+		return (NULL);
+	total_size = size * nmemb;
+	dest = malloc(total_size);
+	if (!dest)
+		return (NULL);
+	while (i < (int)total_size)
+    {
+        ((char *)dest)[i] = 0;
+        i++;
+    }
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	size_t	full_len;
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	full_len = ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1;
+	dest = malloc(sizeof(char) * full_len);
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+    {
+        dest[i] = s1[i];
+        i++;
+    }
+	while (s2[j])
+    {
+        dest[i + j] = s2[j];
+        j++;
+    }
+    dest[full_len - 1] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s && *s != (char)c)
+	{
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
